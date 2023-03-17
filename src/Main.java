4869f65b1125d22f279fcc5bf850e832dfa28d1d
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-17 16:31:32 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Välj ut ett slumpvis namn från en fil och skriv ut det på skärmen
+        //   - Klura ut hur många rader det finns i filen
+        //   - Slumpa fram en rad
+        //   - Visa namnet från just den raden
+
+
+    }
+}
